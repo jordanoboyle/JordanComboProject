0841ced78c79bfbef3325e1b98629a86b18eb2b3
jordanoboyle <jmcmcode@gmail.com>
2024-12-02 22:27:04 -0700

    HTML for the cube

--- App/Scripts/landingPageScript.js
+++ App/Scripts/landingPageScript.js
@@ -1,11 +1,15 @@
 //DOM Elements
 const toggleNav = document.getElementById('toggle-nav');
+const totalContainer = document.querySelector('.total-container');
 const close     = document.getElementById('close');
 const open      = document.getElementById('open');
 const modalOne  = document.getElementById('modal-one');
+const body      = document.body;
 
 
 //Toggle the Nav Bar:
 toggleNav.addEventListener('click', () => {
   document.body.classList.toggle('show-nav');
+  totalContainer.classList.toggle('nav-open');
+  
 })
